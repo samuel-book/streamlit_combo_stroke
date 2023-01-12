c2884043327db784aef485fe704a3ccd48de1eb9
Anna Laws <a.laws2@exeter.ac.uk>
2023-01-12 11:39:13 +0000

    Minor edit to other project/cite/resources pages

--- pages/4_Project.py
+++ pages/4_Project.py
@@ -5,8 +5,5 @@ from utilities_top.inputs import write_text_from_file
 
 page_setup()
 
-if __name__ == '__main__':
-
-    write_text_from_file('pages/text_for_pages/4_Project.txt',
-                     head_lines_to_skip=2)
-    st.write('oi oi')
+write_text_from_file('pages/text_for_pages/4_Project.txt',
+                    head_lines_to_skip=2)
