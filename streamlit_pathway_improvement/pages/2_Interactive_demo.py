c56756d9af58bfafc43c36d53c057e55656fc520
Anna Laws <a.laws2@exeter.ac.uk>
2023-01-16 16:05:01 +0000

    git subrepo pull streamlit_pathway_improvement
    subrepo:
      subdir:   "streamlit_pathway_improvement"
      merged:   "623bd9a"
    upstream:
      origin:   "git@github.com:samuel-book/streamlit_pathway_improvement.git"
      branch:   "main"
      commit:   "623bd9a"
    git-subrepo:
      version:  "0.4.5"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "dbb99be"

--- streamlit_pathway_improvement/pages/2_Interactive_demo.py
+++ streamlit_pathway_improvement/pages/2_Interactive_demo.py
@@ -38,99 +38,104 @@ from utilities_pathway.inputs import \
     write_text_from_file
 
 
-# ###########################
-# ##### START OF SCRIPT #####
-# ###########################
-page_setup()
+def main():
+    # ###########################
+    # ##### START OF SCRIPT #####
+    # ###########################
+    page_setup()
 
-# Title:
-st.markdown('# Pathway improvement')
-
-
-# ###########################
-# ########## SETUP ##########
-# ###########################
-
-df = pd.read_csv(
-    dir + 'data_pathway/scenario_results.csv',
-    # index_col=index_col,
-    header='infer'
-    )
-
-# Baseline_good_outcomes_(median)
-# Baseline_good_outcomes_per_1000_patients_(low_5%)
-# Baseline_good_outcomes_per_1000_patients_(high_95%)
-# Baseline_good_outcomes_per_1000_patients_(mean)
-# Baseline_good_outcomes_per_1000_patients_(stdev)
-# Baseline_good_outcomes_per_1000_patients_(95ci)
-# Percent_Thrombolysis_(median%)
-# Percent_Thrombolysis_(low_5%)
-# Percent_Thrombolysis_(high_95%)
-# Percent_Thrombolysis_(mean)
-# Percent_Thrombolysis_(stdev)
-# Percent_Thrombolysis_(95ci)
-# Additional_good_outcomes_per_1000_patients_(median)
-# Additional_good_outcomes_per_1000_patients_(low_5%)
-# Additional_good_outcomes_per_1000_patients_(high_95%)
-# Additional_good_outcomes_per_1000_patients_(mean)
-# Additional_good_outcomes_per_1000_patients_(stdev)
-# Additional_good_outcomes_per_1000_patients_(95ci)
-# Onset_to_needle_(mean)
-# calibration
-# scenario
-# stroke_team
-
-# Pull out the list of stroke teams:
-stroke_team_list = sorted(set(df['stroke_team'].to_numpy()))
-
-# Select a team
-team_input = st.selectbox(
-    'Pick a team',
-    stroke_team_list
-    )
-
-# Pick out just the data for the chosen team:
-df_here = df[df['stroke_team'] == team_input]
+    # Title:
+    st.markdown('# Pathway improvement')
 
 
+    # ###########################
+    # ########## SETUP ##########
+    # ###########################
 
-# ###########################
-# ######### RESULTS #########
-# ###########################
+    df = pd.read_csv(
+        dir + 'data_pathway/scenario_results.csv',
+        # index_col=index_col,
+        header='infer'
+        )
 
-# Make a bar chart of the mean values:
-import plotly.graph_objects as go
+    # Baseline_good_outcomes_(median)
+    # Baseline_good_outcomes_per_1000_patients_(low_5%)
+    # Baseline_good_outcomes_per_1000_patients_(high_95%)
+    # Baseline_good_outcomes_per_1000_patients_(mean)
+    # Baseline_good_outcomes_per_1000_patients_(stdev)
+    # Baseline_good_outcomes_per_1000_patients_(95ci)
+    # Percent_Thrombolysis_(median%)
+    # Percent_Thrombolysis_(low_5%)
+    # Percent_Thrombolysis_(high_95%)
+    # Percent_Thrombolysis_(mean)
+    # Percent_Thrombolysis_(stdev)
+    # Percent_Thrombolysis_(95ci)
+    # Additional_good_outcomes_per_1000_patients_(median)
+    # Additional_good_outcomes_per_1000_patients_(low_5%)
+    # Additional_good_outcomes_per_1000_patients_(high_95%)
+    # Additional_good_outcomes_per_1000_patients_(mean)
+    # Additional_good_outcomes_per_1000_patients_(stdev)
+    # Additional_good_outcomes_per_1000_patients_(95ci)
+    # Onset_to_needle_(mean)
+    # calibration
+    # scenario
+    # stroke_team
 
-cols_bar = st.columns(2)
-with cols_bar[0]:
-    # Percentage thrombolysis use:
-    fig = go.Figure()
-    fig.add_trace(go.Bar(
-        x=df_here['scenario'],
-        y=df_here['Percent_Thrombolysis_(mean)']
-    ))
+    # Pull out the list of stroke teams:
+    stroke_team_list = sorted(set(df['stroke_team'].to_numpy()))
 
-    fig.update_xaxes(title='Scenario')
-    fig.update_yaxes(title='Percent Thrombolysis (mean)')
+    # Select a team
+    team_input = st.selectbox(
+        'Pick a team',
+        stroke_team_list
+        )
 
-    st.plotly_chart(fig, use_container_width=True)
-
-with cols_bar[1]:
-    # Additional good outcomes 
-    fig = go.Figure()
-    fig.add_trace(go.Bar(
-        x=df_here['scenario'],
-        y=df_here['Additional_good_outcomes_per_1000_patients_(mean)'],
-        marker=dict(color='red')
-    ))
-
-    fig.update_xaxes(title='Scenario')
-    fig.update_yaxes(title='Additional_good_outcomes_per_1000_patients_(mean)')
-
-    st.plotly_chart(fig, use_container_width=True)
+    # Pick out just the data for the chosen team:
+    df_here = df[df['stroke_team'] == team_input]
 
 
-st.write('Full data for this team: ')
-st.write(df_here)
 
-# ----- The end! -----
+    # ###########################
+    # ######### RESULTS #########
+    # ###########################
+
+    # Make a bar chart of the mean values:
+    import plotly.graph_objects as go
+
+    cols_bar = st.columns(2)
+    with cols_bar[0]:
+        # Percentage thrombolysis use:
+        fig = go.Figure()
+        fig.add_trace(go.Bar(
+            x=df_here['scenario'],
+            y=df_here['Percent_Thrombolysis_(mean)']
+        ))
+
+        fig.update_xaxes(title='Scenario')
+        fig.update_yaxes(title='Percent Thrombolysis (mean)')
+
+        st.plotly_chart(fig, use_container_width=True)
+
+    with cols_bar[1]:
+        # Additional good outcomes 
+        fig = go.Figure()
+        fig.add_trace(go.Bar(
+            x=df_here['scenario'],
+            y=df_here['Additional_good_outcomes_per_1000_patients_(mean)'],
+            marker=dict(color='red')
+        ))
+
+        fig.update_xaxes(title='Scenario')
+        fig.update_yaxes(title='Additional_good_outcomes_per_1000_patients_(mean)')
+
+        st.plotly_chart(fig, use_container_width=True)
+
+
+    st.write('Full data for this team: ')
+    st.write(df_here)
+
+    # ----- The end! -----
+
+
+if __name__ == '__main__':
+    main()
