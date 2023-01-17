95b78bf8afe8b50796a1fce3abf2293238344fb9
Anna Laws <a.laws2@exeter.ac.uk>
2023-01-17 17:07:33 +0000

    git subrepo pull streamlit_pathway_improvement
    subrepo:
      subdir:   "streamlit_pathway_improvement"
      merged:   "96e8742"
    upstream:
      origin:   "git@github.com:samuel-book/streamlit_pathway_improvement.git"
      branch:   "main"
      commit:   "96e8742"
    git-subrepo:
      version:  "0.4.5"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "dbb99be"

--- streamlit_pathway_improvement/Introduction.py
+++ streamlit_pathway_improvement/Introduction.py
@@ -3,8 +3,8 @@ Streamlit app for the stroke outcome model.
 """
 import streamlit as st
 
-from utilities.fixed_params import page_setup
-from utilities.inputs import write_text_from_file
+from utilities_pathway.fixed_params import page_setup
+from utilities_pathway.inputs import write_text_from_file
 
 page_setup()
 
--- streamlit_pathway_improvement/pages/6_Resources.py
+++ streamlit_pathway_improvement/pages/6_Resources.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-from utilities.fixed_params import page_setup
-from utilities.inputs import write_text_from_file
+from utilities_pathway.fixed_params import page_setup
+from utilities_pathway.inputs import write_text_from_file
 
 page_setup()
 
