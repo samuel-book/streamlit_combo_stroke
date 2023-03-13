52fd64d6a25864c7016c6f80c6feba6f71ac453d
Anna Laws <a.laws2@exeter.ac.uk>
2023-03-13 09:26:20 +0000

    Test of wide markdown table on Resources page

--- pages/7_Project.py
+++ pages/7_Project.py
@@ -5,11 +5,6 @@ from utilities_top.inputs import write_text_from_file
 
 page_setup()
 
-st.warning(''.join([
-    ':warning: __To do:__ ',
-    'The text on this page needs updating for the final app.'
-]))
-
 write_text_from_file('pages/text_for_pages/4_Project.txt',
                     head_lines_to_skip=2)
                    
