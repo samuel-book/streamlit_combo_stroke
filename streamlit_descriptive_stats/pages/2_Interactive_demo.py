6ba9df699c71fc3d7f2c1611dd3449893378dde4
Anna Laws <a.laws2@exeter.ac.uk>
2023-09-26 13:09:08 +0100

    git subrepo pull streamlit_descriptive_stats
    subrepo:
      subdir:   "streamlit_descriptive_stats"
      merged:   "99f3596"
    upstream:
      origin:   "https://github.com/samuel-book/streamlit_descriptive_stats"
      branch:   "main"
      commit:   "99f3596"
    git-subrepo:
      version:  "0.4.5"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "dbb99be"

--- streamlit_descriptive_stats/pages/2_Interactive_demo.py
+++ streamlit_descriptive_stats/pages/2_Interactive_demo.py
@@ -10,57 +10,61 @@ done in functions stored in files named container_(something).py
 # ----- Imports -----
 import streamlit as st
 import pandas as pd
-import numpy as np
+# import numpy as np
 
 # Custom functions:
-from utilities.fixed_params import page_setup
-from utilities.inputs import \
-    write_text_from_file
+from utilities_descriptive.fixed_params import page_setup
+# from utilities.inputs import \
+#     write_text_from_file
 # Containers:
-import utilities.container_inputs
-import utilities.container_results
-import utilities.container_details
+# import utilities.container_inputs
+# import utilities.container_results
+# import utilities.container_details
 
 
-# ###########################
-# ##### START OF SCRIPT #####
-# ###########################
-page_setup()
+def main():
+    # ###########################
+    # ##### START OF SCRIPT #####
+    # ###########################
+    page_setup()
 
-# Title:
-st.markdown('# Descriptive statistics')
+    # Title:
+    st.markdown('# Descriptive statistics')
 
-# ###########################
-# ########## SETUP ##########
-# ###########################
-st.header('Setup')
-st.subheader('Inputs')
+    # ###########################
+    # ########## SETUP ##########
+    # ###########################
+    st.header('Setup')
+    st.subheader('Inputs')
 
-stroke_team_list = pd.read_csv(
-    './data/stroke_teams.csv', index_col=False).sort_values('stroke_team')
+    stroke_team_list = pd.read_csv(
+        './data/stroke_teams.csv', index_col=False).sort_values('stroke_team')
 
-stroke_teams_selected = st.multiselect('Stroke team', options=stroke_team_list)
+    stroke_teams_selected = st.multiselect(
+        'Stroke team', options=stroke_team_list)
 
-limit_to_4hr = st.toggle('Limit to arrival within 4hr')
+    limit_to_4hr = st.toggle('Limit to arrival within 4hr')
+
+    if limit_to_4hr:
+        summary_stats_file = 'summary_stats_4hr.csv'
+    else:
+        summary_stats_file = 'summary_stats.csv'
+
+    summary_stats_df = pd.read_csv(
+        './data/' + summary_stats_file, index_col=0
+    )
+
+    teams_to_show = ['all E+W'] + stroke_teams_selected
+
+    # ###########################
+    # ######### RESULTS #########
+    # ###########################
+    st.header('Results')
+
+    st.write(summary_stats_df[teams_to_show])
+
+    # ----- The end! -----
 
 
-if limit_to_4hr:
-    summary_stats_file = 'summary_stats_4hr.csv'
-else:
-    summary_stats_file = 'summary_stats.csv'
-
-summary_stats_df = pd.read_csv(
-    './data/' + summary_stats_file, index_col=0
-)
-
-teams_to_show = ['all E+W'] + stroke_teams_selected
-
-
-# ###########################
-# ######### RESULTS #########
-# ###########################
-st.header('Results')
-
-st.write(summary_stats_df[teams_to_show])
-
-# ----- The end! -----
+if __name__ == '__main__':
+    main()
